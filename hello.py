12e941ecb8fa81a693eb459373920ce8be28c7b9
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-05-30 22:49:39 +0900

    fix

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import math 
 import numpy as np
-x = np.linspace(0,10,100)
-y = x
-plt.plot(x,y)
-plt.show()
+x = np.linspace(-10,10,100)
+y = x*x
+st.pyplot(x,y)
+
