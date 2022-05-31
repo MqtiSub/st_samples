142130e9ea58f6f017640573f78924e4792516f8
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-05-31 12:11:30 +0900

    Create

---
+++ Third.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import math 
+import numpy as np
+st.title("Linear function")
+graph_a = st.slider('coordinateOfA',-4,4,0)
+graph_b = st.slider('coordinateOfB',-4,4,0)
+graph_c = st.slider('coordinateOfC',-4,4,0)
+graph_d = st.slider('coordinateOfD',-4,4,0)
+graph_x = np.linspace(-8,8,100)
+graph_y = graph_a*graph_x**3+graph_b*graph_x**2+graph_c*graph_x+graph_d
+fig = plt.figure()
+plt.plot(graph_x,graph_y)
+st.pyplot(fig)
