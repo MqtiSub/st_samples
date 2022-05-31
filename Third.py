7580ea9ec19282430b42c2e3ed8c62301bdbba6d
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-05-31 12:43:26 +0900

    ver1.0.2

--- Third.py
+++ Third.py
@@ -9,6 +9,9 @@ graph_c = st.slider('coordinateOfC',-4,4,0)
 graph_d = st.slider('coordinateOfD',-4,4,0)
 graph_x = np.linspace(-8,8,100)
 graph_y = graph_a*graph_x**3+graph_b*graph_x**2+graph_c*graph_x+graph_d
+plt.xlabel('x')  # 横軸のラベル
+plt.ylabel('y',  rotation=0)  # 縦軸のラベル
+plt.grid()  # グリッド（目盛り線）を表示
 fig = plt.figure()
 plt.plot(graph_x,graph_y)
 st.pyplot(fig)
