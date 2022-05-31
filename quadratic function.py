669cac253b6f75fc02aa8056be696580a47c2f50
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-05-31 12:35:53 +0900

    Try

--- quadratic function.py
+++ quadratic function.py
@@ -8,8 +8,6 @@ graph_b = st.slider('coordinateOfB',-4,4,0)
 graph_c = st.slider('coordinateOfC',-4,4,0)
 graph_x = np.linspace(-8,8,100)
 graph_y = graph_a*graph_x*graph_x+graph_b*graph_x+graph_c
-plt.xlabel('x')  # 横軸のラベル
-plt.ylabel('y',  rotation=0)  # 縦軸のラベル
 plt.grid()  # グリッド（目盛り線）を表示
 fig = plt.figure()
 plt.plot(graph_x,graph_y)
