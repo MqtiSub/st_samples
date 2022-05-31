775b9c1fa47d844320764770d02743fd78ccff62
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-05-31 11:14:03 +0900

    ver1.0.1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import math 
 import numpy as np
+%matplotlib inline
 st.title("Linear function")
 graph_a = st.slider('coordinateOfA',-4,4,0)
 graph_b = st.slider('coordinateOfB',-4,4,0)
@@ -10,5 +11,3 @@ graph_y = graph_a*graph_x+graph_b
 fig = plt.figure()
 plt.plot(graph_x,graph_y)
 st.pyplot(fig)
-
-
