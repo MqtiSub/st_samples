56f8bbffdcd74390edff2f4ce03a5ef50d4af32f
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-06-27 13:05:50 +0900

    Update mendel.py

--- mendel.py
+++ mendel.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import random
+i = 0
+a = 0
+st.title("mendel thought")
+number = st.number_input('append number for counts')
+while i < number:
+  i += 1
+  if random.randint(1,4) == 1:
+    a += 1
+
+st.write(a/number)    
+  
 
